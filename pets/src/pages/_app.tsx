b1718d2809e77a96b3d12f7cefaf17927478770e
Matheus <matheusn06leite@gmail.com>
2025-01-03 10:01:40 -0300

    Criando o cabeçalho com a logo

--- pets/src/pages/_app.tsx
+++ pets/src/pages/_app.tsx
@@ -2,13 +2,15 @@ import "../ui/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material";
 import tema from "../ui/themes/tema";
+import Cabecalho from "../ui/components/head/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={tema}>
+      <Cabecalho />
       <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
 
 export default MyApp;
---
+++ pets/src/ui/components/head/head.tsx
@@ -0,0 +1,9 @@
+import { CabecalhoContainer, Logo } from "./head.style";
+
+export default function Cabecalho() {
+  return (
+    <CabecalhoContainer>
+      <Logo src="/imagens/logo.svg" alt="Adote um Pet" />
+    </CabecalhoContainer>
+  );
+}
